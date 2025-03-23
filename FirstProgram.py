9baa552a4bfb9c923704d2022bb2043b79e9efbd
v1dya-sagar <vidyasagar08123@gmail.com>
2025-03-23 19:37:10 +0530

    test commit

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,1 +1,28 @@
-print("HelloWorld")
+# i=1
+# while i < 10:
+#     i += 1
+#     print(i)
+# a=b=c=1
+# print(id(a))
+# print(id(b))
+# print(id(c))
+# b=10
+# print(id(a))
+# print(id(b))
+# print(id(c))
+
+# print(bool(0))
+# a = 1
+# print(a)
+
+for x in range(1, 10):
+    print(x-1)
+
+
+
+
+
+
+
+
+
